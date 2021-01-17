5e324b3733030a6e1c85a911c04baf3373607931
Guthias <gmgustavom@gmail.com>
2021-01-17 12:23:12 -0300

    Clicking shows the value

--- src/index.js
+++ src/index.js
@@ -2,17 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component{
+class Square extends React.Component{  
   render() {
-    return(
-     <div className="square">
-       {this.props.value}
-     </div> 
-    );
+    if(this.props.state === 0){
+      return(
+        <div className="square" onClick={() => this.props.onClick()}></div>
+      );
+    } else{
+      return(
+        <div className="square active">
+          {this.props.value}
+        </div> 
+       );
+    }
+    
   }
 }
 
 class Board extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      bombTable: this.createBoard(8, 8, 10),
+      clickTable: this.emptyBoard(8, 8, 10),
+    }
+    
+  }
+  emptyBoard(lengthX, lengthY){
+    const emptyTable = Array(lengthY);
+    for(let i = 0; i < emptyTable.length; i++){
+      emptyTable[i] = new Array(lengthX).fill(0);
+    }
+    return emptyTable;
+  }
+
   createBoard(lengthX, lengthY, numBomb){
     const boardTable = Array(lengthY);
     for(let i = 0; i < boardTable.length; i++){
@@ -68,108 +91,113 @@ class Board extends React.Component{
 
     for(let y = 0; y < lengthY; y++){
       for(let x = 0; x < lengthX; x++){
-        boardTable[y][x] == 0 ? boardTable[y][x] = null: boardTable[y][x] = boardTable[y][x];
+        if(boardTable[y][x] === 0){ boardTable[y][x] = null};
       }
     }
-
-
     return boardTable;
   }
 
-  renderSquare(i){
-    return <Square value={i}/>
+  squareClick(x, y){
+    const clickTable = this.state.clickTable.slice();
+    clickTable[y][x] = 1
+    console.log("x: ", x, "y: ", y)
+    this.setState({
+      clickTable: clickTable,
+    });
+  }
+  renderSquare(i, c, y, x){
+    return <Square value={i} state={c} onClick={() => this.squareClick(x, y)}/>
   }
   render() {
-    const boardTable = this.createBoard(8, 8, 10);
-
+    
     return(
       <div className="board">
         <div className="board-row">
-          {this.renderSquare(boardTable[0][0])}
-          {this.renderSquare(boardTable[0][1])}
-          {this.renderSquare(boardTable[0][2])}
-          {this.renderSquare(boardTable[0][3])}
-          {this.renderSquare(boardTable[0][4])}
-          {this.renderSquare(boardTable[0][5])}
-          {this.renderSquare(boardTable[0][6])}
-          {this.renderSquare(boardTable[0][7])}
+          {this.renderSquare(this.state.bombTable[0][0], this.state.clickTable[0][0], 0, 0)}
+          {this.renderSquare(this.state.bombTable[0][1], this.state.clickTable[0][1], 0, 1)}
+          {this.renderSquare(this.state.bombTable[0][2], this.state.clickTable[0][2], 0, 2)}
+          {this.renderSquare(this.state.bombTable[0][3], this.state.clickTable[0][3], 0, 3)}
+          {this.renderSquare(this.state.bombTable[0][4], this.state.clickTable[0][4], 0, 4)}
+          {this.renderSquare(this.state.bombTable[0][5], this.state.clickTable[0][5], 0, 5)}
+          {this.renderSquare(this.state.bombTable[0][6], this.state.clickTable[0][6], 0, 6)}
+          {this.renderSquare(this.state.bombTable[0][7], this.state.clickTable[0][7], 0, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[1][0])}
-          {this.renderSquare(boardTable[1][1])}
-          {this.renderSquare(boardTable[1][2])}
-          {this.renderSquare(boardTable[1][3])}
-          {this.renderSquare(boardTable[1][4])}
-          {this.renderSquare(boardTable[1][5])}
-          {this.renderSquare(boardTable[1][6])}
-          {this.renderSquare(boardTable[1][7])}
+          {this.renderSquare(this.state.bombTable[1][0], this.state.clickTable[1][0], 1, 0)}
+          {this.renderSquare(this.state.bombTable[1][1], this.state.clickTable[1][1], 1, 1)}
+          {this.renderSquare(this.state.bombTable[1][2], this.state.clickTable[1][2], 1, 2)}
+          {this.renderSquare(this.state.bombTable[1][3], this.state.clickTable[1][3], 1, 3)}
+          {this.renderSquare(this.state.bombTable[1][4], this.state.clickTable[1][4], 1, 4)}
+          {this.renderSquare(this.state.bombTable[1][5], this.state.clickTable[1][5], 1, 5)}
+          {this.renderSquare(this.state.bombTable[1][6], this.state.clickTable[1][6], 1, 6)}
+          {this.renderSquare(this.state.bombTable[1][7], this.state.clickTable[1][7], 1, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[2][0])}
-          {this.renderSquare(boardTable[2][1])}
-          {this.renderSquare(boardTable[2][2])}
-          {this.renderSquare(boardTable[2][3])}
-          {this.renderSquare(boardTable[2][4])}
-          {this.renderSquare(boardTable[2][5])}
-          {this.renderSquare(boardTable[2][6])}
-          {this.renderSquare(boardTable[2][7])}
+          {this.renderSquare(this.state.bombTable[2][0], this.state.clickTable[2][0], 2, 0)}
+          {this.renderSquare(this.state.bombTable[2][1], this.state.clickTable[2][1], 2, 1)}
+          {this.renderSquare(this.state.bombTable[2][2], this.state.clickTable[2][2], 2, 2)}
+          {this.renderSquare(this.state.bombTable[2][3], this.state.clickTable[2][3], 2, 3)}
+          {this.renderSquare(this.state.bombTable[2][4], this.state.clickTable[2][4], 2, 4)}
+          {this.renderSquare(this.state.bombTable[2][5], this.state.clickTable[2][5], 2, 5)}
+          {this.renderSquare(this.state.bombTable[2][6], this.state.clickTable[2][6], 2, 6)}
+          {this.renderSquare(this.state.bombTable[2][7], this.state.clickTable[2][7], 2, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[3][0])}
-          {this.renderSquare(boardTable[3][1])}
-          {this.renderSquare(boardTable[3][2])}
-          {this.renderSquare(boardTable[3][3])}
-          {this.renderSquare(boardTable[3][4])}
-          {this.renderSquare(boardTable[3][5])}
-          {this.renderSquare(boardTable[3][6])}
-          {this.renderSquare(boardTable[3][7])}
+          {this.renderSquare(this.state.bombTable[3][0], this.state.clickTable[3][0], 3, 0)}
+          {this.renderSquare(this.state.bombTable[3][1], this.state.clickTable[3][1], 3, 1)}
+          {this.renderSquare(this.state.bombTable[3][2], this.state.clickTable[3][2], 3, 2)}
+          {this.renderSquare(this.state.bombTable[3][3], this.state.clickTable[3][3], 3, 3)}
+          {this.renderSquare(this.state.bombTable[3][4], this.state.clickTable[3][4], 3, 4)}
+          {this.renderSquare(this.state.bombTable[3][5], this.state.clickTable[3][5], 3, 5)}
+          {this.renderSquare(this.state.bombTable[3][6], this.state.clickTable[3][6], 3, 6)}
+          {this.renderSquare(this.state.bombTable[3][7], this.state.clickTable[3][7], 3, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[4][0])}
-          {this.renderSquare(boardTable[4][1])}
-          {this.renderSquare(boardTable[4][2])}
-          {this.renderSquare(boardTable[4][3])}
-          {this.renderSquare(boardTable[4][4])}
-          {this.renderSquare(boardTable[4][5])}
-          {this.renderSquare(boardTable[4][6])}
-          {this.renderSquare(boardTable[4][7])}
+          {this.renderSquare(this.state.bombTable[4][0], this.state.clickTable[4][0], 4, 0)}
+          {this.renderSquare(this.state.bombTable[4][1], this.state.clickTable[4][1], 4, 1)}
+          {this.renderSquare(this.state.bombTable[4][2], this.state.clickTable[4][2], 4, 2)}
+          {this.renderSquare(this.state.bombTable[4][3], this.state.clickTable[4][3], 4, 3)}
+          {this.renderSquare(this.state.bombTable[4][4], this.state.clickTable[4][4], 4, 4)}
+          {this.renderSquare(this.state.bombTable[4][5], this.state.clickTable[4][5], 4, 5)}
+          {this.renderSquare(this.state.bombTable[4][6], this.state.clickTable[4][6], 4, 6)}
+          {this.renderSquare(this.state.bombTable[4][7], this.state.clickTable[4][7], 4, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[5][0])}
-          {this.renderSquare(boardTable[5][1])}
-          {this.renderSquare(boardTable[5][2])}
-          {this.renderSquare(boardTable[5][3])}
-          {this.renderSquare(boardTable[5][4])}
-          {this.renderSquare(boardTable[5][5])}
-          {this.renderSquare(boardTable[5][6])}
-          {this.renderSquare(boardTable[5][7])}
+          {this.renderSquare(this.state.bombTable[5][0], this.state.clickTable[5][0], 5, 0)}
+          {this.renderSquare(this.state.bombTable[5][1], this.state.clickTable[5][1], 5, 1)}
+          {this.renderSquare(this.state.bombTable[5][2], this.state.clickTable[5][2], 5, 2)}
+          {this.renderSquare(this.state.bombTable[5][3], this.state.clickTable[5][3], 5, 3)}
+          {this.renderSquare(this.state.bombTable[5][4], this.state.clickTable[5][4], 5, 4)}
+          {this.renderSquare(this.state.bombTable[5][5], this.state.clickTable[5][5], 5, 5)}
+          {this.renderSquare(this.state.bombTable[5][6], this.state.clickTable[5][6], 5, 6)}
+          {this.renderSquare(this.state.bombTable[5][7], this.state.clickTable[5][7], 5, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[6][0])}
-          {this.renderSquare(boardTable[6][1])}
-          {this.renderSquare(boardTable[6][2])}
-          {this.renderSquare(boardTable[6][3])}
-          {this.renderSquare(boardTable[6][4])}
-          {this.renderSquare(boardTable[6][5])}
-          {this.renderSquare(boardTable[6][6])}
-          {this.renderSquare(boardTable[6][7])}
+          {this.renderSquare(this.state.bombTable[6][0], this.state.clickTable[6][0], 6, 0)}
+          {this.renderSquare(this.state.bombTable[6][1], this.state.clickTable[6][1], 6, 1)}
+          {this.renderSquare(this.state.bombTable[6][2], this.state.clickTable[6][2], 6, 2)}
+          {this.renderSquare(this.state.bombTable[6][3], this.state.clickTable[6][3], 6, 3)}
+          {this.renderSquare(this.state.bombTable[6][4], this.state.clickTable[6][4], 6, 4)}
+          {this.renderSquare(this.state.bombTable[6][5], this.state.clickTable[6][5], 6, 5)}
+          {this.renderSquare(this.state.bombTable[6][6], this.state.clickTable[6][6], 6, 6)}
+          {this.renderSquare(this.state.bombTable[6][7], this.state.clickTable[6][7], 6, 7)}
         </div>
 
         <div className="board-row">
-          {this.renderSquare(boardTable[7][0])}
-          {this.renderSquare(boardTable[7][1])}
-          {this.renderSquare(boardTable[7][2])}
-          {this.renderSquare(boardTable[7][3])}
-          {this.renderSquare(boardTable[7][4])}
-          {this.renderSquare(boardTable[7][5])}
-          {this.renderSquare(boardTable[7][6])}
-          {this.renderSquare(boardTable[7][7])}
+          {this.renderSquare(this.state.bombTable[7][0], this.state.clickTable[7][0], 7, 0)}
+          {this.renderSquare(this.state.bombTable[7][1], this.state.clickTable[7][1], 7, 1)}
+          {this.renderSquare(this.state.bombTable[7][2], this.state.clickTable[7][2], 7, 2)}
+          {this.renderSquare(this.state.bombTable[7][3], this.state.clickTable[7][3], 7, 3)}
+          {this.renderSquare(this.state.bombTable[7][4], this.state.clickTable[7][4], 7, 4)}
+          {this.renderSquare(this.state.bombTable[7][5], this.state.clickTable[7][5], 7, 5)}
+          {this.renderSquare(this.state.bombTable[7][6], this.state.clickTable[7][6], 7, 6)}
+          {this.renderSquare(this.state.bombTable[7][7], this.state.clickTable[7][7], 7, 7)}
         </div>
       </div>
     );
